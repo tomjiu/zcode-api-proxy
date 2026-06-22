ce0a00cbfa900c6fd4bd7200b9466e6a6226e62c
TriDefender <nitric.trioxide@gmail.com>
2026-06-22 11:51:35 +0000

    chore: bumped version

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import { readFileSync, existsSync, writeFileSync } from "node:fs";
 import { join } from "node:path";
 import { homedir } from "node:os";
 
-const VERSION = "2.0.0";
+const VERSION = "2.0.1";
 
 main();
 
